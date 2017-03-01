95608bfda9b94bcc4da7b3a12008ef971d6d2d87
aryan <aryan@HP-ENVY-X360>
2017-03-01 17:09:53 +0000

    Working on more info buttons

--- SquareRoute/script.js
+++ SquareRoute/script.js
@@ -1,6 +1,19 @@
 function main() {
     $('#fade').hide();
-    $('#fade').fadeIn(5000);
+    $('#fade').fadeIn(3000);
+
+    $('.MoreButtonA').click(function () {
+        $('.submenuA').slideToggle();
+    });
+
+    $('.MoreButtonJ').click(function () {
+        $('.submenuJ').slideToggle();
+    });
+
+    $('.MoreButtonB').click(function () {
+        $('.submenuB').slideToggle();
+    });
 }
 
 $(document).ready(main);
+
