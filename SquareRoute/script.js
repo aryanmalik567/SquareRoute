8f910dc67258396ba893738adaf021b6771d1a2d
aryan <aryan@HP-ENVY-X360>
2017-03-01 17:31:16 +0000

    More buttons almost done

--- SquareRoute/script.js
+++ SquareRoute/script.js
@@ -2,15 +2,21 @@ function main() {
     $('#fade').hide();
     $('#fade').fadeIn(3000);
 
+    $('.submenuA').hide();
     $('.MoreButtonA').click(function () {
+        $('.MoreButtonA').show();
         $('.submenuA').slideToggle();
     });
 
+    $('.submenuJ').hide();
     $('.MoreButtonJ').click(function () {
+        $('.MoreButtonJ').show();
         $('.submenuJ').slideToggle();
     });
 
+    $('.submenuB').hide();
     $('.MoreButtonB').click(function () {
+        $('.MoreButtonB').show();
         $('.submenuB').slideToggle();
     });
 }
